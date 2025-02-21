baf3539b44e5c542b1711c3533016abe0baaeb4b
hex-FF43C7 <wortmand000@alpinesd.org>
2025-02-21 13:50:20 -0700

    name getter added, not tested

--- code/LinkManager.py
+++ code/LinkManager.py
@@ -1,8 +1,13 @@
 import re
 import yt_dlp
+from yt_dlp.utils import download_range_func
+from yt_dlp.postprocessor import FFmpegPostProcessor
+import requests
+from bs4 import BeautifulSoup
+import os
 
 class link:
-  def __init__(self, line, in_path, out_path):
+  def __init__(self, line, out_path):
     key = r"^(?P<link>(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-]))( \(((?P<start>(\d\d:\d\d:\d\d)|[B,b]), ?(?P<stop>(\d\d:\d\d:\d\d)|[E,e]))\))?$"
     # goups link, start, stop
     
@@ -22,21 +27,51 @@ class link:
     except AttributeError:
       self.stop = 'E'
 
-    self.in_path = in_path
     self.out_path = out_path
+    self.name = self.get_name()
 
-    def download(self):
-      yt_opts = {
-          'outtmpl': self.out_path,
-          'recode-video': 'mp4',
-          'nocheckcertificate': True,
-          'postprocessors': [{
-              'key': 'FFmpegVideoConvertor',
-              'preferedformat': 'mp4'
-          }]
-      }
-      with yt_dlp.YoutubeDL(yt_opts) as ydl:
-        ydl.download(self.link)
+  def get_name(self):
+    r = requests.get(self.link)
+    soup = BeautifulSoup(r.text)
+
+    link = soup.find_all(name="title")[0]
+    title = str(link)
+    title = title.replace("<title>", "")
+    title = title.replace("</title>", "")
+    title = title.replace(" - YouTube", "")
+    title = re.replace(r'^\(\d*\) ?', '', title)
+    title = title.replace(" ", "_")
+    title = re.sub(r'[<>:"/\\|?*\.]', '', title)
+
+    return title
+
+  def download(self):
+    if self.start == 'B':
+      start_time = 0
+    else:
+      start_time = self.start
+    
+    if self.stop == 'E':
+      end_time = None
+    else: 
+      end_time = self.stop
+    
+    ffmpeg_path = '/bin/ffmpeg'
+    FFmpegPostProcessor._ffmpeg_location.set(ffmpeg_path)
+
+    yt_opts = {
+        'outtmpl': os.path.normpath(self.out_path, f'{self.name}.%(ext)s'),
+        'recode-video': 'mp4',
+        'nocheckcertificate': True,
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4'
+        }],
+        'download_ranges': download_range_func(None, [(start_time, end_time)]),
+        'force_keyframes_at_cuts': True,
+    }
+    with yt_dlp.YoutubeDL(yt_opts) as ydl:
+      ydl.download(self.link)
       
 
 
